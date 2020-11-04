34855cbd6cc4bed5405db89949de4cf2a07c089e
tiennv3 <tiennv3@users.noreply.github.com>
2020-11-04 09:53:36 +0700

    update button

--- lua-frontend/src/config.ts
+++ lua-frontend/src/config.ts
@@ -1,7 +1,7 @@
 export default {
   rpc: process.env.NODE_ENV === 'production' ? 
     'https://wallet.tomochain.com/api/luaswap/rpc' :
-    'http://localhost:8020/rpc',
+    'https://mainnet.infura.io/v3/d81b764e1175473ab36844831dc02fa0',
   chainId: 1,
   api: process.env.NODE_ENV === 'production' ? 
   'https://wallet.tomochain.com/api/luaswap' :
